8ba7062ba3ff574c853fb677038eb5ce1ac92598
gabrielbritto89 <gabrielbritto.bio@gmail.com>
2026-02-12 00:43:52 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,2 @@
-function Home(){return <h1>"Nada na vida pode ser obtido sem uma espécie de sacrifício.".</h1>}
+function Home(){return <h1>"Nada na vida pode ser obtido sem uma espécie de sacrifício." FMA.</h1>}
 export default Home;
